555e6f79022893bc2a9aeada085d532484847603
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-06-26 22:41:40 +0530

    add images

---
+++ src/component/Login.js
@@ -0,0 +1,25 @@
+// import React from "react";
+import styled from "styled-components";
+
+const Login = (props) => {
+  return (
+    <Container>
+      <Content>Contenet</Content>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  overflow : hidden;
+  display:flex;
+  flex-diraction:column;
+  text-align:center;
+  hight:100vh;
+
+`
+const Content =styled.div`
+  margin-bottom : 10vw;
+  
+
+`
+export default Login;
