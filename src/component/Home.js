1f4ca080eeb79d950bec0027813236eff5aec950
darshansatapara <109288106+darshansatapara@users.noreply.github.com>
2023-06-29 22:03:19 +0530

    Home page sliderbar is complate

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,36 @@
 import styled from "styled-components";
-const Home =(props)=>{
-    return(
-        <div>
-            Home
-        </div>
-    )
-}
+import ImgSlider from "./ImageSlider";
+
+
+const Home = (props) => {
+    return (
+        <Container>
+            <ImgSlider />
+        </Container>
+    );
+
+
+};
+
+//style
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh);
+  overflow-x: hidden;
+  display: block;
+  padding-top: 70px;
+  padding: 0 calc(3.5vw +5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
+
 export default Home;
---
+++ src/component/ImageSlider.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import styled from "styled-components";
+
+const ImgSlider = (props) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <a>
+          <img src="/images/slider-badging.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="/images/slider-badag.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="/images/slider-scale.jpg" alt="" />
+        </a>
+      </Wrap>
+      <Wrap>
+        <a>
+          <img src="/images/slider-scales.jpg" alt="" />
+        </a>
+      </Wrap>
+    </Carousel>
+  );
+};
+
+const Carousel = styled(Slider)`
+  margin: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+  .slick-list {
+    overflow: initial;
+  }
+  .slick-prev {
+    right: -75px;
+  }
+`;
+
+const Wrap = styled.div`
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+
+  img {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relative;
+    padding: 4px;
+   height: 100%;
+   width: 100%;
+
+   &:hover{
+    border-radius: 4px;
+    border: 4px solid white;
+    transition-duration: 100ms;
+   }
+   
+  }
+`;
+export default ImgSlider;
